2fb5f45587fd3f38d21856c8b5d6812da6472d30
TatenHarrison <816295@mystma.org>
2020-09-14 20:18:11 +0000

    top ten movies

---
+++ Unit1/src/app/App.java
@@ -0,0 +1,17 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("the shrekoning");
+        System.out.println("killer bean");
+        System.out.println("the wasp movie");
+        System.out.println("shrek 5");
+        System.out.println("The terrible Super Mario Movie");
+        System.out.println("The Mega Blox movie");
+        System.out.println("Titanic 2");
+        System.out.println("Atlantic rim");
+        System.out.println("Departure");
+        System.out.println("The first Jedi");
+
+    }
+}
