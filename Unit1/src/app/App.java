7e4a79c7d67e63bf2d36003c238d253f740b8911
TatenHarrison <816295@mystma.org>
2020-09-29 20:17:23 +0000

    Quiz

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,33 +1,51 @@
 package app;
-
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for (int i = 10; i <= 20; i = i + 1){
-            System.out.println("the count is " + i);
-            System.out.println("Lets keep counting");
+        Scanner input =new Scanner(System.in);
+
+        System.out.println("How many times do they say the word bee in the bee movie?");
+        int answer1 = input.nextInt();
+        if (answer1 == 174) {
+            System.out.println("You are correct!");
+        } else {
+            System.out.println("You are incorrect...");
         }
 
-        for (int i2 = 10; i2 <= 100; i2 = i2 + 1){
-            System.out.println("the count is " + i2);
-            System.out.println("lets keep counting");
+        System.out.println("What is 8-4?");
+        int answer2 = input.nextInt();
+        if (answer2 == 4) {
+            System.out.println("Good job, you are a smart.");
+        } else {
+            System.out.println("You are wrong, because if you take away the 4, it's no longer there, so all that is left is an 8.");
         }
 
-        for (int i3 = 1000; i3 <= 256000; i3 = i3 + 1){
-            System.out.println("the count is " + i3);
-            System.out.println("lets keep counting");
+        System.out.println("Would you like a hard-boiled egg?");
+        String answer3 = input.nextLine();
+        if (answer3.equals("no")) {
+            System.out.println("Good");
+        } else {
+            System.out.println("How dare you");
         }
 
-        for (int i4 = 11; i4 <= 12; i4 = i4 + 1){
-            System.out.println("the count is " + i4);
-            System.out.println("lets keep counting");
+        System.out.println("Who do you main in Smash Bros. Ultimate?");
+        String answer4 = input.nextLine();
+        if (answer4.equals("Little Mac")) {
+            System.out.println("I'm not mad, just dissapointed...");
+        } else {
+            System.out.println("Cool.");
         }
 
-        for (int i5 = 10; i5 >= 0; i5 = i5 - 1){
-            System.out.println("the count is " + i5);
-            System.out.println("lets keep subtracting");
+        System.out.println("Apple or Samsung?");
+        String answer5 = input.nextLine();
+        if (answer5.equals("Apple")) {
+            System.out.println("Nice");
+        } else {
+            System.out.println("no.");
         }
+
 
     } 
 }
