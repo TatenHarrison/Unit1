e9a7aaaeb49f1ecdc4e401f4fb8d1ebd95a789fc
TatenHarrison <816295@mystma.org>
2020-09-30 19:53:27 +0000

    Mad libs project number 2

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -4,47 +4,21 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Scanner input =new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("How many times do they say the word bee in the bee movie?");
-        int answer1 = input.nextInt();
-        if (answer1 == 174) {
-            System.out.println("You are correct!");
-        } else {
-            System.out.println("You are incorrect...");
-        }
+        System.out.println("Enter a name");
+        String name = input.nextLine();
 
-        System.out.println("What is 8-4?");
-        int answer2 = input.nextInt();
-        if (answer2 == 4) {
-            System.out.println("Good job, you are a smart.");
-        } else {
-            System.out.println("You are wrong, because if you take away the 4, it's no longer there, so all that is left is an 8.");
-        }
+        System.out.println("enter a movie");
+        String movie = input.nextLine();
 
-        System.out.println("Would you like a hard-boiled egg?");
-        String answer3 = input.nextLine();
-        if (answer3.equals("no")) {
-            System.out.println("Good");
-        } else {
-            System.out.println("How dare you");
-        }
+        System.out.println("enter an adjective");
+        String adjective = input.nextLine();
 
-        System.out.println("Who do you main in Smash Bros. Ultimate?");
-        String answer4 = input.nextLine();
-        if (answer4.equals("Little Mac")) {
-            System.out.println("I'm not mad, just dissapointed...");
-        } else {
-            System.out.println("Cool.");
-        }
+        System.out.println("enter another name");
+        String name2 = input.nextLine();
 
-        System.out.println("Apple or Samsung?");
-        String answer5 = input.nextLine();
-        if (answer5.equals("Apple")) {
-            System.out.println("Nice");
-        } else {
-            System.out.println("no.");
-        }
+        System.out.println(name + "'s favorite movie is " + movie + ". They watch it every day with their " + adjective + " friend, " + name2 + ".");
 
 
     } 
