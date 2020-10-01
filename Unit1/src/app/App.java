f898af99e1cdcfd8fff8c3b1cbc719cc4c1b13b1
TatenHarrison <816295@mystma.org>
2020-10-01 15:44:04 +0000

    animal facts

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,20 +5,25 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter a number 1-5 to hear a fact about dogs");
+        int number = input.nextInt();
 
-        System.out.println("Enter a name");
-        String name = input.nextLine();
-
-        System.out.println("enter a movie");
-        String movie = input.nextLine();
-
-        System.out.println("enter an adjective");
-        String adjective = input.nextLine();
-
-        System.out.println("enter another name");
-        String name2 = input.nextLine();
-
-        System.out.println(name + "'s favorite movie is " + movie + ". They watch it every day with their " + adjective + " friend, " + name2 + ".");
+        if (number == 1) {
+            System.out.println("3 dogs survived the sinking of the titanic.");
+        }
+        if (number == 2) {
+            System.out.println("A bloodhound's sense of smell can be used as evidence in court.");
+        }
+        if (number == 3) {
+            System.out.println("Dogs have 3 eyelids");
+        }
+        if (number == 4) {
+            System.out.println("a dog's nose print is as unique as a human fingerprint");
+        }
+        if (number == 5) {
+            System.out.println("when dogs curl up, it's not only to keep warm, but also to protect vital organs.");
+        }
 
 
     } 
