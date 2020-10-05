1399c1080f483f095d53033de17e9c9905ccb856
TatenHarrison <816295@mystma.org>
2020-10-05 19:50:27 +0000

    challenge 2 day one

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,10 +1,17 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        for (int bruh = 0; bruh <= 1000; bruh = bruh + 1){
-            System.out.println(bruh);
+        Scanner input = new Scanner(System.in);
+        System.out.println("What's the password?");
+        String password = input.nextLine();
+
+        if (password == "Shrek"){
+            System.out.println("Somebody once told me the world was gonna roll me, I aint the sharpest tool in the shed. She was looking kinda dumb with her finger and her thumb in the shape of an L on her forehead. Well, the years start coming and they dont stop coming, fed to the rules and I hit the ground running. Didnt make sense not to live for fun, your brain gets smart but your head gets dumb. So much to do, so much to see, so whats wrong with taking the backstreets? You never know if you dont go, you never shine if you dont glow. Hey now, youre an all star, get your game on, go play. Hey now, youre a rockstar, get the show on, get paid. And all that glitters is gold, only shooting stars break the mold. Its a cold place, and they say it gets colder. Youre bundeled up now wait til you get older, but the meteor man beg to differ, judging by the hole in the satellite picture. The ice we skate is getting pretty thin, the waters getting warm so you might as well swim. My worlds on fire, how bout yours, thats the way I like it and I never get bored. Hey now, youre an all star, get your game on, go play. Hey now, youre a rockstar, get the show on, get paid. And all that glitters is gold, only shooting stars break the mold. Hey now, youre an all star, get your game on, go play. Hey now, youre a rockstar, get the show on, get paid. And all that glitters is gold, only shooting stars break the mold. ");
         }
+
         
 
     } 
