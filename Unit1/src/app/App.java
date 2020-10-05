09bb172abea347fc92b24a64c9da6203af4061a6
TatenHarrison <816295@mystma.org>
2020-10-05 19:36:31 +0000

    challenge number one

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,34 +2,9 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        boolean beeMovie = false;
-        byte fingers = 9;
-        char shrek = 'S';
-        short seconds = 3600;
-        int cookies = 19000;
-        long cookies2 = 1900000000;
-        double twentyFivePercent = 0.25;
-        double oneDividedBy3Extended = 0.3333333333;
-
-        System.out.println("Do I own a copy of the bee movie?");
-        System.out.println(beeMovie);
-        System.out.println("How many fingers does a nine-fingered man have?");
-        System.out.println(fingers);
-        System.out.println("What does fiona's husband's name start with?");
-        System.out.println(shrek);
-        System.out.println("How many seconds is in an hour?");
-        System.out.println(seconds);
-        System.out.println("How many cookies does the average american eat in a lifetime?");
-        System.out.println(cookies);
-        System.out.println("How many cookies do 100000000000000 people eat in their lifetimes combined?");
-        System.out.println(cookies2);
-        System.out.println("What is 1 divided by 4?");
-        System.out.println(twentyFivePercent);
-        System.out.println("What is 1 divided by 3?");
-        System.out.println(oneDividedBy3Extended);
-
-
-        
+        for (int bruh = 0; bruh <= 1000; bruh = bruh + 1){
+            System.out.println(bruh);
+        }
         
 
     } 
