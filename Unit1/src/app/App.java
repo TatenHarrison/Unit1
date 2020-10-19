5b61cdc41297f535aeb955c0025d6c25235eab3a
TatenHarrison <816295@mystma.org>
2020-10-19 20:17:48 +0000

    Roulette project day 1

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,26 +1,53 @@
 package app;
 import java.util.Scanner;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number");
-        int numberOne = input.nextInt();
-        System.out.println("Enter another number");
-        int numberTwo = input.nextInt();
+        
+        System.out.println("Would you like to make an inside or outside bet?");
+        String inOrOut = input.nextLine();
 
-        while (numberOne <= numberTwo) {
-            System.out.println(numberOne);
-            numberOne = numberOne + 2;
+        if (inOrOut.equals("inside")){
+            System.out.println("What number would you like to bet on?");
+            int insideNum = input.nextInt();
+            System.out.println("How much would you like to bet?");
+            int bet1 = input.nextInt();
+            System.out.println("You will bet $" + bet1 + " on " + insideNum + ".");
         }
+        if (inOrOut.equals("outside")){
+                System.out.println("Would you like to bet on even, odd, black, or red?");
+                String outsideChoice = input.nextLine();
 
-      
+                if (outsideChoice.equals("even")){
+                    System.out.println("How much would you like to bet?");
+                }
 
+                if (outsideChoice.equals("odd")){
+                    System.out.println("How much would you like to bet?");
+                }
+
+                if (outsideChoice.equals("black")){
+                    System.out.println("How much would you like to bet?");
+                }
+
+                if (outsideChoice.equals("red")){
+                    System.out.println("How much would you like to bet?");
+                }
+            }
         
         
-
         
+        
+        
+        
+        
+        Random rnd = new Random();
+         
+        int n1 = rnd.nextInt(35);
+        
+         
     }
     
 }
