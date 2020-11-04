be5d96a2a98e11c44c57a5c2cd0636c04f60720f
TatenHarrison <816295@mystma.org>
2020-11-04 21:20:54 +0000

    roulette project 5464562

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -14,45 +14,57 @@ public class App {
         int bet2;
         int bet3;
         int bet4;
+        int numBets = 0;
+        boolean win = false;
 
         int betss[] = new int[10];
-        String betType[] = {"even", "odd", "red", "black"}
+        String betType2[] = new String[10];
+        String betType[] = { "even", "odd", "red", "black" };
 
+        
 
-         for (int i = 0;i < betss.length; i++){
+        for (int i = 0; i < betss.length; i++) {
             if (retrybet == false) {
                 System.out.println("Would you like to make an inside or outside bet?");
                 String inOrOut = input.nextLine();
                 int black[] = { 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35 };
                 int red[] = { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
+                int odd[] = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35};
+                int even[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36};
 
                 if (inOrOut.equals("inside")) {
                     System.out.println("What number would you like to bet on?");
-                    insideNum = input.nextInt();
+                    betType2[i] = input.nextLine();
                     System.out.println("How much would you like to bet?");
                     betss[i] = input.nextInt();
-                    System.out.println("You will bet $" + betss[i] + " on " + insideNum + ".");
+                    input.nextLine();
+                    System.out.println("You will bet $" + betss[i] + " on " + betType2[i] + ".");
+                    numBets++;
 
                 }
                 if (inOrOut.equals("outside")) {
                     System.out.println("Would you like to bet on even, odd, black, or red?");
-                    outsideChoice = input.nextLine();
+                    betType2[i] = input.nextLine();
+                    numBets++;
 
-                    if (outsideChoice.equals("even")) {
+                    if (betType2[i].equals("even")) {
                         System.out.println("How much would you like to bet?");
                         betss[i] = input.nextInt();
+                        input.nextLine();
                         System.out.println("You will bet $" + betss[i] + " on even.");
 
                     }
-                    if (outsideChoice.equals("odd")) {
+                    if (betType2[i].equals("odd")) {
                         System.out.println("How much would you like to bet?");
                         betss[i] = input.nextInt();
+                        input.nextLine();
                         System.out.println("You will bet $" + betss[i] + " on odd.");
 
                     }
-                    if (outsideChoice.equals("black")) {
+                    if (betType2[i].equals("black")) {
                         System.out.println("How much would you like to bet?");
                         betss[i] = input.nextInt();
+                        input.nextLine();
                         System.out.println("You will bet $" + betss[i] + " on black.");
 
                     }
@@ -63,20 +75,30 @@ public class App {
                 if (retry.equals("no")) {
                     retrybet = true;
                 }
+
+            }
+        }
+        if (retrybet == true) {
+            for (int i = 0; i < numBets; i++) {
+                System.out.println("You bet $" + betss[i] + " on " + betType2[i]);
+                }
+            }
+            Random rnd = new Random();
+            int n1 = rnd.nextInt(37);
+            System.out.println("The number was " + n1 + ".");
+            for (int i = 0; i < betss.length; i++) {
+                if (n1 == betss[i]) {
+                    win = true;
+                }
+            }
+            if (win == true) {
+                    System.out.println("You win!");
+                } 
+            else {
+                    System.out.println("You lose...");
+                }
             
-            }
         }
-        if (retrybet == true){
-            for ( int i = 0; i < betss.length; i++){
-                System.out.println("You bet " + betss[i] + "on ");
-            }
-        Random rnd = new Random();
-        int n1 = rnd.nextInt(37);
-        System.out.println("The number was " + n1 + ".");
-        }
-        
-        
 
     }
 
-}
